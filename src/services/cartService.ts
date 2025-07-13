99486f2922bc097bc3884f4b3cd88279641b5b58
Zeyad Elsayed <Zeyad.elsayed6251@gmail.com>
2025-07-14 02:41:45 +0300

    Creating 'Updating Items in cart' route

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -52,7 +52,7 @@ export const addItemToCart = async ({ productId, quantity, userId }: AddItemToCa
         return { data: "Low stock for item", statusCode: 400 }
     }
 
-    product.stock -= parseInt(quantity) 
+    //product.stock -= parseInt(quantity) 
 
     cart.items.push({ 
         product: productId, 
@@ -64,6 +64,48 @@ export const addItemToCart = async ({ productId, quantity, userId }: AddItemToCa
     cart.totalAmount += product.price * parseInt(quantity);
 
     const updatedCart = await cart.save();
-    await product.save();
+    //await product.save();
+    return {data: updatedCart, statusCode: 200};
+}
+
+interface UpdateItemInCart {
+    productId: any;
+    quantity: string;
+    userId: string;
+}
+
+export const updateItemInCart = async ({ productId, quantity, userId }: UpdateItemInCart) => {
+    const cart = await getActiveCartForUser({ userId })
+
+    const existsInCart = (await cart).items.find((p) => p.product.toString() === productId)
+
+    if(!existsInCart)
+        return { data: "Item doesn't exist in cart!", statusCode: 400 }
+
+    const product = await productModel.findById(productId);
+
+    if (!product) {
+        return { data: "Product not found!", statusCode: 400 }
+    }
+
+    if(product.stock < parseInt(quantity)) {
+        return { data: "Low stock for item", statusCode: 400 }
+    }
+
+    
+    const otherCartItems = cart.items.filter((p => p.product.toString() != productId))
+
+    let total = otherCartItems.reduce((sum, product) => {
+        sum += product.quantity * product.unitPrice;
+        return sum;
+    }, 0)
+
+    existsInCart.quantity = parseInt(quantity)
+    total += existsInCart.quantity * existsInCart.unitPrice;
+    cart.totalAmount = total
+
+    const updatedCart = await cart.save();
+
+
     return {data: updatedCart, statusCode: 200};
 }
